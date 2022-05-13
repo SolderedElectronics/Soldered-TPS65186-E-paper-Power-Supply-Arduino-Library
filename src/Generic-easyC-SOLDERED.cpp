79c71c28ea84081d5b93578dc1c8d07f2a6b4c96
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-05-13 11:31:44 +0200

    Backup
    a

--- src/Generic-easyC-SOLDERED.cpp
+++ src/Generic-easyC-SOLDERED.cpp
@@ -17,7 +17,7 @@
  *
  * @param int _pin          Example parameter.
  */
-Sensor::Sensor(int _pin)
+TPS65186::TPS65186(int _pin)
 {
     pin = _pin;
     native = 1;
@@ -26,7 +26,89 @@ Sensor::Sensor(int _pin)
 /**
  * @brief                   Overloaded function for virtual in base class to initialize sensor specific.
  */
-void Sensor::initializeNative()
+void TPS65186::initializeNative()
 {
     pinMode(pin, INPUT);
 }
+
+bool TPS65186::begin()
+{
+
+    EasyC::begin();
+}
+
+bool TPS65186::enableRail(int rail)
+{
+    Wire.beginTransmission(0x48);
+    uint8_t temp;
+    readRegister(0x01, &temp, 1);
+    Wire.write(0x01);
+    Wire.write(temp | rail);
+    return !(Wire.endTransmission());
+}
+
+bool TPS65186::disableRail(int rail)
+{
+    Wire.beginTransmission(0x48);
+    uint8_t temp;
+    readRegister(0x01, &temp, 1);
+    Wire.write(0x01);
+    Wire.write(temp & ~(rail));
+    return !(Wire.endTransmission());
+}
+
+bool TPS65186::voltageAdjust(int voltage)
+{
+    Wire.beginTransmission(0x48);
+    Wire.write(0x0);
+    Wire.write(voltage);
+}
+
+
+
+/*
+    Wire.beginTransmission(0x48);
+    Wire.write(0x09);
+    Wire.write(B00011011); // Power up seq.
+    Wire.write(B00000000); // Power up delay (3mS per rail)
+    Wire.write(B00011011); // Power down seq.
+    Wire.write(B00000000); // Power down delay (6mS per rail)
+    Wire.endTransmission();
+
+
+ // Put TPS65186 into standby mode (leaving 3V3 SW active)
+    VCOM_CLEAR;
+    Wire.beginTransmission(0x48);
+    Wire.write(0x01);
+    Wire.write(0x6f);
+    Wire.endTransmission();
+
+    // Wait for all PWR rails to shut down
+    delay(100);
+
+    // Disable 3V3 to the panel
+    Wire.beginTransmission(0x48);
+    Wire.write(0x01);
+    Wire.write(0x4f);
+    Wire.endTransmission();
+
+     // Enable all rails
+    Wire.beginTransmission(0x48);
+    Wire.write(0x01);
+    Wire.write(B00101111);
+    Wire.endTransmission();
+
+    // Modify power up sequence  (VEE and VNEG are swapped)
+    Wire.beginTransmission(0x48);
+    Wire.write(0x09);
+    Wire.write(B11100001);
+    Wire.endTransmission();
+
+    delay(1);
+
+    // Switch TPS65186 into active mode
+    Wire.beginTransmission(0x48);
+    Wire.write(0x01);
+    Wire.write(B10101111);
+    Wire.endTransmission();
+*/
--- src/Generic-easyC-SOLDERED.h
+++ src/Generic-easyC-SOLDERED.h
@@ -9,19 +9,33 @@
  * @authors     @ soldered.com
  ***************************************************/
 
-#ifndef __SENSOR__
-#define __SENSOR__
+#ifndef __TPS65186__
+#define __TPS65186__
 
 #include "Arduino.h"
 #include "libs/Generic-easyC/easyC.hpp"
 
-class Sensor : public EasyC
+#define V3P3   1<<5
+#define VCOM   1<<4
+#define VDDH   1<<3
+#define VPOS   1<<2
+#define VEE    1<<1
+#define VNEG   1<<0
+
+#define V1500 011
+#define V1475 100
+#define V1450 101
+#define V1425 110
+
+
+class TPS65186 : public EasyC
 {
   public:
-    Sensor(int _pin);
+    TPS65186(int _pin);
 
   protected:
     void initializeNative();
+    bool begin();
 
   private:
     int pin;
